7ad448d76a6c6f00287c48b6007376d82e5c6722
yuq <chinayu9iang@gmail.com>
2019-04-29 16:42:41 +0800

    FAIR-2441 补充环境搭建流程

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,10 +2,10 @@
  * 配置文件
  */
 
-export const sourceIp = '127.0.0.1';
+export const sourceIp = window.location.host;
 
 // 资源服务器port 有些手机浏览器显示端口号无法识别  全部使用默认端口
-// export const sourcePort = pi_update.severPort || '80';
+export const sourcePort = window.location.port;
 
 // erlang逻辑服务器ip
 // app.herominer.net
