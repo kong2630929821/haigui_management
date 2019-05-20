513708dd867d0d2eb55d09023ff6a1cbe9e73f9f
liul <948884112@qq.com>
2019-05-20 15:50:11 +0800

    HGYH-96  管理后台-订单筛选查询及导入导出

--- src/app/view/page/totalOrders.ts
+++ src/app/view/page/totalOrders.ts
@@ -22,8 +22,18 @@ export class TotalOrder extends Widget {
         supplierList:['供应商id'],
         orderType:['已下单未支付','已支付未发货','已发货未签收','已收货'],
         exportType:['未导出','已导出'],
-        timeType:['下单时间','支付时间','收货时间','发货时间','完成时间']
+        timeType:['下单时间','支付时间','收货时间','发货时间','完成时间'],
+        active:0,
+        supplierActive:0,
+        orderTypeActive:0,
+        exportActive:0,
+        timeTypeActive:0,
+        start:0,
+        tail:1558334628335,
+        inputOrderId:0,
+        orderList:[]
     };
+
     public create() {
         // 切换到所有订单页时将所有供应商查询出来
         getAllSupplier().then((r) => {
@@ -37,22 +47,9 @@ export class TotalOrder extends Widget {
         });
     }
     public exportOrder(e:any) {
-        // 获取一个供应商id
-        const dom = getRealNode(e.node).parentNode.children[2];
-        const supplierId = dom.value;
-        if (supplierId === '供应商id') {
-            alert('请选择供应商');
-
-            return;
-        }
-        // 根据供应商id获取未发货的订单
-        getOrder(Number(supplierId),2).then((jsonData) => {
-            console.log('jsonData=',jsonData);
-            const jsonHead = [
-                'supplierId','id', 'uid',	'labels',	'origin',	 'tax',	'freight',	'other',	'name', 	'tel', 	'area',	'address',	'order_time',	'pay_time',	'ship_time'	,'receipt_time',	'finish_time' ,'物流单号'
-            ];
-            jsonToExcelConvertor(jsonHead,jsonData,'未发货订单');
-        });
+        const jsonHead = ['选择','订单编号','商品ID','商品名称','商品SKU','商品规格','供货商ID','用户ID','姓名','手机号','地址信息','订单状态','物流单号'];
+        const jsonData = this.props.contentList;
+        jsonToExcelConvertor(jsonHead,jsonData,'订单');
     }
 
     public importTransport(e:any) {
@@ -64,7 +61,7 @@ export class TotalOrder extends Widget {
     }
     // 按订单id查询
     public searchById(e:any) {
-        const orderId = Number(getRealNode(e.node).parentNode.children[1].value);
+        const orderId = Number(this.props.inputOrderId);
         if (!orderId) {
             alert('订单不存在');
 
@@ -96,49 +93,7 @@ export class TotalOrder extends Widget {
             this.paint();
         });
     }
-    public test(e:any) {
-        const dom = getRealNode(e.node);
-        debugger;
-    }
-    // 显示某供应商指定类型订单
-    public showOrder(e:any) {
-        const dom = getRealNode(e.node);
-        const orderType = dom.parentNode.children[3].options.selectedIndex + 1;
-        const supplierId = Number(dom.parentNode.children[2].value);
-        getOrder(supplierId,orderType).then((r) => {
-            console.log('r= ',r);
-            if (!r) return Promise.reject('！！！请求接口错误');
-            
-            return JSON.parse(r);// 根据供应商和订单类型筛选得到订单id数组
-        }).then((r) => {
-            for (let i = 0;i < r.length;i++) {
-                const orderId = r[i][1];
-                getOrderById(orderId).then((r) => { // 根据id把订单查询出来并显示
-                    console.log('r= ',r);
-                    const orderInfo = JSON.parse(r);
-                    const contentList = [];
-                    for (let i = 0;i < orderInfo.length;i++) {
-                        let orderState = '';
-                        if (orderInfo[i][11] === 0) {
-                            orderState = '待付款';
-                        } else if (orderInfo[i][12] === 0) {
-                            orderState = '待发货';
-                        } else if (orderInfo[i][13] === 0) {
-                            orderState = '待收货';
-                        } else if (orderInfo[i][15] === 0) {
-                            orderState = '已完成';
-                        }
-                        
-                        const temp = [];
-                        temp.push(0,orderInfo[0][0],orderInfo[0][12],orderInfo[0][15],orderInfo[0][16],orderInfo[0][17],orderInfo[0][18],orderInfo[0][1],orderInfo[0][2],orderInfo[0][3],orderInfo[0][4],orderState);
-                        contentList.push(temp);
-                    }
-                    this.props.contentList = contentList;
-                    this.paint();
-                });
-            }
-        }).catch((e) => {console.log(e);});
-    }
+    
     // 获取订单
     public showAllOrder(argsList:any) {
         const id = 0;// 订单id,等于0表示从最大开始获取，大于0表示从指定订单id开始获取
@@ -153,66 +108,61 @@ export class TotalOrder extends Widget {
         return getAllOrder(id,count,time_type,start,tail,sid,orderType,state);
     }
         // 根据供应商id筛选
-    public showOrderBySid(e:any) {
-        const supplierId = Number(getRealNode(e.node).value);
-        const orderType = getRealNode(e.node).parentNode.children[3].options.selectedIndex + 1;
-        const exportType = getRealNode(e.node).parentNode.children[4].options.selectedIndex;
-        const timeDom = getRealNode(e.node).parentNode.children[5].children;
-        const start = Date.parse(timeDom[0].value);
-        const tail = Date.parse(timeDom[1].value);
-        const timeType = timeDom[2].options.selectedIndex + 1;
+    public filterSupplierId(e:any) {
+        this.props.supplierActive = e.value;
+        const supplierId = Number(this.props.supplierList[this.props.supplierActive]);
+        const orderType = this.props.orderTypeActive;
+        const exportType = this.props.exportActive;
+        const start = this.props.start;
+        const tail = this.props.tail;
+        const timeType = this.props.timeTypeActive + 1;
         const argsList = [supplierId,orderType,exportType,start,tail,timeType];
         this.showAllOrder(argsList).then((orderList) => {
-            // 显示订单
             this.showOrderOnPage(orderList);
         });
     }
 
         // 根据订单类型筛选
-    public showOrderByOrderType(e:any) {
-        const supplierId = Number(getRealNode(e.node).parentNode.children[2].value);
-        const orderType = Number(getRealNode(e.node).options.selectedIndex + 1);
-        const exportType = getRealNode(e.node).parentNode.children[4].options.selectedIndex;
-        const timeDom = getRealNode(e.node).parentNode.children[5].children;
-        const start = Date.parse(timeDom[0].value);
-        const tail = Date.parse(timeDom[1].value);
-        const timeType = timeDom[2].options.selectedIndex + 1;
+    public filterOrderType(e:any) {
+        this.props.orderTypeActive = e.value;
+        const supplierId = Number(this.props.supplierList[this.props.supplierActive]);
+        const orderType = this.props.orderTypeActive;
+        const exportType = this.props.exportActive;
+        const start = this.props.start;
+        const tail = this.props.tail;
+        const timeType = this.props.timeTypeActive + 1;
         const argsList = [supplierId,orderType,exportType,start,tail,timeType];
         this.showAllOrder(argsList).then((orderList) => {
-            // 显示订单
             this.showOrderOnPage(orderList);
         });
     }
 
         // 根据导出状态筛选,未导出，已导出
-    public showOrderByExportState(e:any) {
-        const supplierId = Number(getRealNode(e.node).parentNode.children[2].value);
-        const orderType = getRealNode(e.node).parentNode.children[3].options.selectedIndex + 1;
-        const exportType = Number(getRealNode(e.node).options.selectedIndex);
-        const timeDom = getRealNode(e.node).parentNode.children[5].children;
-        const start = Date.parse(timeDom[0].value);
-        const tail = Date.parse(timeDom[1].value);
-        const timeType = timeDom[2].options.selectedIndex + 1;
+    public filterExportType(e:any) {
+        this.props.exportActive = e.value;
+        const supplierId = Number(this.props.supplierList[this.props.supplierActive]);
+        const orderType = this.props.orderTypeActive;
+        const exportType = this.props.exportActive;
+        const start = this.props.start;
+        const tail = this.props.tail;
+        const timeType = this.props.timeTypeActive + 1;
         const argsList = [supplierId,orderType,exportType,start,tail,timeType];
         this.showAllOrder(argsList).then((orderList) => {
-            // 显示订单
             this.showOrderOnPage(orderList);
         });
     }
 
         // 根据时间筛选
-    public showOrderByTime(e:any) {
-        const timeDom = getRealNode(e.node).parentNode.children;
-        const start = Date.parse(timeDom[0].value);
-        const tail = Date.parse(timeDom[1].value);
-        const otherDom = getRealNode(e.node).parentNode.parentNode.children;
-        const supplierId = Number(otherDom[2].value);
-        const orderType = otherDom[3].options.selectedIndex + 1;
-        const exportType = Number(otherDom[4].options.selectedIndex);
-        const timeType = timeDom[2].options.selectedIndex + 1;
+    public filterTimeType(e:any) {
+        this.props.timeTypeActive = e.value;
+        const supplierId = Number(this.props.supplierList[this.props.supplierActive]);
+        const orderType = this.props.orderTypeActive;
+        const exportType = this.props.exportActive;
+        const start = this.props.start;
+        const tail = this.props.tail;
+        const timeType = this.props.timeTypeActive + 1;
         const argsList = [supplierId,orderType,exportType,start,tail,timeType];
         this.showAllOrder(argsList).then((orderList) => {
-            // 显示订单
             this.showOrderOnPage(orderList);
         });
     }
@@ -252,5 +202,14 @@ export class TotalOrder extends Widget {
             this.props.contentList = contentList;
             this.paint();
         }
+    }
+    public startTime(e:any) {
+        // debugger;
+        // this.props.start = e.value;
+    }
+
+    public inputValue(e:any) {
+        this.props.inputOrderId = e.value;
+        this.paint();
     }
 }
